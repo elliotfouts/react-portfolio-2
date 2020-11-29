b0e17d63b9a983d8994aad268ba6cdb4c3daafdf
Elliot Fouts <elliotfouts@gmail.com>
2020-11-28 16:34:44 -0800

    added favicon and modified link to resume to not use href

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Resume from "../../assets/Elliot_Fouts_Resume.pdf"
 import "./index.css";
 
 export default function Menu(props) {
+  const [isVisible, setIsVisible] = useState(false)
+
+  // animate words in 
+  useEffect(() => {
+    setTimeout(() => setIsVisible(true), 1000);
+  });
+
+  // href was not working
+  function handleResumeClick() {
+    window.open(Resume);
+  }
 
   return (
-    <div className="menu-container">
-      {/* <h2 className="menu-title">About</h2> */}
-      <a href={Resume} rel="noreferrer" target="_blank" className="menu-link resume">Resume</a>
-      {/* <a className="menu-link">Who is this guy?</a> */}
-
-      {/* <h2 className="menu-title">Connect</h2> */}
+    <div className={`menu-container ${isVisible && "visible"}`}>
+      <a onClick={handleResumeClick} className="menu-link resume">Resume</a>
+      
       <a href="https://www.linkedin.com/in/elliotfouts/" rel="noreferrer" target="_blank" className="menu-link">Linkedin</a>
       <a href="https://github.com/elliotfouts" rel="noreferrer" target="_blank" className="menu-link">Github</a>
       <a href="mailto:elliotfouts@gmail.com" rel="noreferrer" className="menu-link">elliotfouts@gmail.com</a>
--- src/components/WelcomeText/index.js
+++ src/components/WelcomeText/index.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./index.css";
 
 export default function WelcomeText(props) {
+  const [isVisible, setIsVisible] = useState(false)
+  // animate words in 
+  useEffect(() => {
+    setTimeout(() => setIsVisible(true), 1000);
+  });
   return (
-    <div className="welcome-text-container">
+    <div className={`welcome-text-container ${isVisible && "visible"}`}>
       <h2 className="welcome-text-subtext">Hello, my name is</h2>
       <h1 className="welcome-text-main-text">Elliot Fouts.</h1>
       <h2 className="welcome-text-subtext">I am a full-stack engineer.</h2>
